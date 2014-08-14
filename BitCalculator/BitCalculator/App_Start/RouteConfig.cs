8886e67ddfb5e12e16dde0e23cea7b670f1c4052
mariangr <marian_graurov@abv.bg>
2014-08-14 22:52:32 +0300

    prevented Default of form

--- BitCalculator/BitCalculator/App_Start/RouteConfig.cs
+++ BitCalculator/BitCalculator/App_Start/RouteConfig.cs
@@ -16,7 +16,12 @@ namespace BitCalculator
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "BitCalculator", id = UrlParameter.Optional }
+
+                defaults: new { 
+                    controller = "Home", 
+                    action = "BitCalculator", 
+                    id = UrlParameter.Optional 
+                }
             );
         }
     }
