860c3ac9dddcca8394ebedef2f9bfcbdbb8198be
Marian Graurov <marian_graurov@abv.bg>
2014-08-14 17:14:50 +0300

    AJAX logic added

--- BitCalculator/BitCalculator/Controllers/HomeController.cs
+++ BitCalculator/BitCalculator/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,18 +11,14 @@ namespace BitCalculator.Controllers
     
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         public ActionResult BitCalculator(DataModel @dataModel)
         {
-            //TO DO get array of table elements and fill the page from new model
-            TableDataModel tableData = new TableDataModel();
-            tableData.GetParams(@dataModel);
-            return View(tableData);
+            @dataModel.setTableData();
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_TablePartial", @dataModel);
+            }
+            return View(@dataModel);
         }
-
     }
 }
--- BitCalculator/BitCalculator/Models/DataModel.cs
+++ BitCalculator/BitCalculator/Models/DataModel.cs
@@ -10,5 +10,35 @@ namespace BitCalculator.Models
         public int Quantity { set; get; }
         public int Type { set; get; }
         public int Kilo { set; get; }
+        public double[] kilos = new double[18];
+
+        public void setTableData()
+        {
+            this.kilos[this.Type] = this.Quantity;
+
+            for (int i = this.Type - 2; i >= 0; i -= 2)
+            {
+                this.kilos[i] = kilos[i + 2] * this.Kilo;
+            }
+            for (int i = this.Type + 2; i < kilos.Length; i += 2)
+            {
+                this.kilos[i] = kilos[i - 2] / this.Kilo;
+            }
+            if (this.Type % 2 == 0)
+            {
+                for (int i = 1; i < this.kilos.Length; i += 2)
+                {
+                    this.kilos[i] = this.kilos[i - 1] / 8;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < this.kilos.Length - 1; i += 2)
+                {
+                    this.kilos[i] = this.kilos[i + 1] * 8;
+                }
+            }
+
+        }
     }
 }
