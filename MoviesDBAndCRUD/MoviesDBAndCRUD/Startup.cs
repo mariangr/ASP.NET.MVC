4280e6bcf6be040580ca5b25752ba5c82b3389f0
Marian Graurov <marian_graurov@abv.bg>
2014-08-18 17:46:58 +0300

    Movie DataBase and CRUD operations

---
+++ MoviesDBAndCRUD/MoviesDBAndCRUD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoviesDBAndCRUD.Startup))]
+namespace MoviesDBAndCRUD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ MoviesDBAndCRUD/SQLQuery2.sql
@@ -0,0 +1,31 @@
+﻿CREATE TABLE Movies
+
+(
+
+       ID int primary key identity (1,1),
+
+       Title varchar(50),
+
+       Director varchar(200),
+
+       Year int,
+
+	   LeadingMaleRole varchar(200),
+
+	   LeadingMaleRoleAge int,
+
+	   LeadingMaleRoleAgeStudio varchar(100),
+
+	   LeadingMaleRoleAgeStudioAddress varchar(500),
+
+	   LeadingFemaleRole varchar(200),
+
+	   LeadingFemaleRoleAge int,
+
+	   LeadingFemaleRoleStudio varchar(100),
+
+	   LeadingFemaleRoleStudioAddress varchar(500)
+
+
+
+)
